88bf1371591e7324639872931c058a5f159821ae
Aiza-malik <157694133+Aiza-malik@users.noreply.github.com>
2024-01-25 15:01:38 +0500

    Add files via upload

---
+++ aiza.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<" enter number"<<endl;
+    cin>>a;
+    if(a>=10)
+        cout<<" number is greater than 10"<<endl;
+    else
+        cout<<" number is less <2 "<<endl;
+    return 0;
+}
+
